6cb45d085eb78f9cd8a6b182e0e89aaa4e5c4b92
Front Phoenix <ho3einzarrabi@gmail.com>
2023-07-21 17:07:42 +0330

    fix(modal provider): modal provider ts error fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default async function RootLayout({
         <ToastProvider />
         <SupabaseProvider>
           <UserProvider>
-            <ModalProvider products={[]} />
+            <ModalProvider />
             <Sidebar songs={userSongs}>{children}</Sidebar>
             <Player />
           </UserProvider>
